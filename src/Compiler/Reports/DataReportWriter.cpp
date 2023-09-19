a6e6f7472afbb24dc653bc1269ff62d81f491390
Oleksandr <apivovarov@quicklogic.com>
2023-09-19 18:11:25 +0300

    DataReportWriter: add comments, formatting, renames

--- src/Compiler/Reports/DataReportWriter.cpp
+++ src/Compiler/Reports/DataReportWriter.cpp
@@ -36,6 +36,7 @@ std::vector<DataReportWriter::ColumnFormatting> DataReportWriter::detectColumnFo
     columnsFormatting.push_back(ColumnFormatting{static_cast<bool>(column.m_alignment & Qt::AlignLeft), column.m_name.size()});
   }
 
+  // find maximum cell size for each column
   for (int i=0; i<header.size(); ++i) {
     for (const QList<QString>& row: dataReport.getData()) {
       if ((i < row.size()) && (i < static_cast<int>(columnsFormatting.size()))) { // in some cases (seen in histogram), for some row we have number of cell is less than in header
@@ -52,40 +53,42 @@ std::vector<DataReportWriter::ColumnFormatting> DataReportWriter::detectColumnFo
 
 void DataReportWriter::write(std::ofstream& file, const IDataReport& dataReport, const std::vector<ColumnFormatting>& columnsFormatting)
 {
-    auto writeRowHelperFn = [](std::ofstream& file, const QList<QString>& row, std::vector<ColumnFormatting> columnsFormatting){
-      ColumnFormatting defaultColumnFormatting{false, 100};
-      ColumnFormatting& columnFormatting = defaultColumnFormatting;
-      for (int i=0; i<row.size(); ++i) {
-        QString cell = row[i];
-        if (i < static_cast<int>(columnsFormatting.size())) {
-          columnFormatting = columnsFormatting[i];
-        } else {
-          columnFormatting = defaultColumnFormatting;
-        }
-
-        int columnSize = columnFormatting.maxSize + DataReportWriter::COLUMN_MARGIN;
-
-        if (columnFormatting.alignLeft) {
-          file << std::setw(columnSize) << std::left << cell.toStdString();
-        } else {
-          int padding = columnSize - cell.length();
-          int leftPadding = padding / 2;
-          int rightPadding = padding - leftPadding;
-            
-          file << std::setw(leftPadding) << ""  << std::left << cell.toStdString() << std::setw(rightPadding) << "";
-        }
+  auto writeRowHelperFn = [](std::ofstream& file, const QList<QString>& row, std::vector<ColumnFormatting> columnsFormatting){
+    ColumnFormatting defaultColumnFormatting{false, 100};
+    ColumnFormatting& columnFormatting = defaultColumnFormatting;
+    for (int i=0; i<row.size(); ++i) {
+      QString cell = row[i];
+      if (i < static_cast<int>(columnsFormatting.size())) {
+        columnFormatting = columnsFormatting[i];
+      } else {
+        columnFormatting = defaultColumnFormatting;
       }
-      file << "\n";
-    };
 
-    QList<QString> columns;
-    for (const ReportColumn& column: dataReport.getColumns()) {
-      columns << column.m_name;
+      int columnSize = columnFormatting.maxSize + DataReportWriter::COLUMN_MARGIN;
+
+      if (columnFormatting.alignLeft) {
+        // left alignment
+        file << std::setw(columnSize) << std::left << cell.toStdString();
+      } else {
+        // center alignment
+        int padding = columnSize - cell.length();
+        int leftPadding = padding / 2;
+        int rightPadding = padding - leftPadding;
+          
+        file << std::setw(leftPadding) << ""  << std::left << cell.toStdString() << std::setw(rightPadding) << "";
+      }
     }
-    writeRowHelperFn(file, columns, columnsFormatting);
-    for (const QList<QString>& lineData: dataReport.getData()) {
-      writeRowHelperFn(file, lineData, columnsFormatting);
-    }
+    file << "\n";
+  };
+
+  QList<QString> header;
+  for (const ReportColumn& column: dataReport.getColumns()) {
+    header << column.m_name;
+  }
+  writeRowHelperFn(file, header, columnsFormatting);
+  for (const QList<QString>& row: dataReport.getData()) {
+    writeRowHelperFn(file, row, columnsFormatting);
+  }
 }
 
 }  // namespace FOEDAG
