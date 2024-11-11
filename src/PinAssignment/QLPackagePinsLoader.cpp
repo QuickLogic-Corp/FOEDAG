609c9a94255d996f9123170d3ec40b035d33f9ba
Oleksandr <apivovarov@quicklogic.com>
2024-11-11 16:03:47 +0200

    fix error: 'QList<T> QSet<T>::toList() const [with T = QString]' is deprecated: Use values() instead.

--- src/PinAssignment/QLPackagePinsLoader.cpp
+++ src/PinAssignment/QLPackagePinsLoader.cpp
@@ -196,7 +196,7 @@ LocationCollisionDetectorPtr QLPackagePinsLoader::validateIOMap(const QString& i
   if (!overlappedLocationToPinsMap.isEmpty()) {
     qWarning() << ioMapFilePath << "contains mapping issues";
     for (auto it = overlappedLocationToPinsMap.begin(); it != overlappedLocationToPinsMap.end(); ++it) {
-      qWarning() << "WARNING: The following pins:" << QStringList(it.value().toList()).join(", ") << "share the same location:" << it.key();
+      qWarning() << "WARNING: The following pins:" << QStringList(it.value().values()).join(", ") << "share the same location:" << it.key();
     }
   }
 
