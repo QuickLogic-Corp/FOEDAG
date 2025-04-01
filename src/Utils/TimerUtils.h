b14e306188c5d17a8158085319d512a38061b3b0
Oleksandr <apivovarov@quicklogic.com>
2025-04-01 11:22:09 +0300

    rename ScopeTimer to TimerUtils where PointTimer is added

--- src/Utils/TimerUtils.h
+++ src/Utils/TimerUtils.h
@@ -19,3 +19,22 @@ private:
     QElapsedTimer m_timer;
 };
 
+
+class PointTimer {
+public:
+    PointTimer()
+        : m_timer() {
+        m_timer.start();
+    }
+
+    ~PointTimer() {
+    }
+
+    void measure(const QString& name) {
+        qDebug() << name << "took" << m_timer.elapsed() << "ms";
+        m_timer.restart();
+    }
+
+private:
+    QElapsedTimer m_timer;
+};
