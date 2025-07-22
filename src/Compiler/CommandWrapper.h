ec0b3a20f0ca4c9c57728675c40e3ea43e535cdd
Oleksandr <apivovarov@quicklogic.com>
2025-07-22 11:36:42 +0300

    start implementing CommandWrapper::matches method

--- src/Compiler/CommandWrapper.h
+++ src/Compiler/CommandWrapper.h
@@ -21,13 +21,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#include "Utils/FileUtils.h"
+
 #include <string>
-#include <vector>
+#include <set>
 #include <filesystem>
+#include <optional>
 #include <regex>
+#include <memory>
 
 
 namespace FOEDAG {
+
+class FileIdentity {
+public:
+  FileIdentity()=default;
+  FileIdentity(const std::filesystem::path& filePath, const std::string& role): m_filePath(filePath), m_role(role) {
+    if (std::filesystem::exists(filePath)) {
+      if (role.empty()) {
+        m_modifiedDateTime = FileUtils::Mtime(filePath);
+      } else {
+        m_md5sum = FileUtils::calcHashFileContent(filePath);
+      }
+    }
+  }
+
+private:
+  std::filesystem::path m_filePath;
+  std::string m_role;
+  std::string m_modifiedDateTime;
+  std::string m_md5sum;
+};
 
 class CommandWrapper {
 public:
@@ -44,9 +68,18 @@ public:
       return thisClean == rhsClean;
   }
 
+  bool matches(const CommandWrapper& rhs, std::string& msg) {
+    if (string() != rhs.string()) {
+      // todo: fill message of diff
+      return false;
+    }
+    return compareFiles(files(), rhs.files(), msg);
+  }
+
   bool empty() const { return m_string.empty(); }
 
   const std::string& string() const { return m_string; }
+  const std::map<std::string, FileIdentity> files() const { return m_files; }
 
   void append(const std::string& parameter, const std::string& value) {
     appendArgument(parameter + " " + value);
@@ -56,26 +89,26 @@ public:
     appendArgument(parameter);
   }
 
-  void appendFile(const std::string& file)=delete;
-  void appendFile(const std::filesystem::path& file) {
-    m_files.push_back(file);
+  void appendFile(const std::string& filePath)=delete;
+  void appendFile(const std::filesystem::path& file, const std::string& role = "") {
+    handleFile(file, role);
     appendArgument(file.string());
   }
-  void watchFiles(const std::vector<std::filesystem::path>& files) {
-    for (const std::filesystem::path& file: files) {
-      watchFile(file);
-    }
-  }
-  void watchFile(const std::filesystem::path& file) {
-    m_watchFiles.push_back(file);
-    // Note: We do not add this file directly to the command-line arguments.
-    // It may be indirectly used within a script file, and the script file itself 
-    // is already included as a command-line argument.    
-  }
+  // void watchFiles(const std::vector<std::filesystem::path>& files) {
+  //   for (const std::filesystem::path& file: files) {
+  //     watchFile(file);
+  //   }
+  // }
+  // void watchFile(const std::filesystem::path& file) {
+  //   m_watchFiles.push_back(file);
+  //   // Note: We do not add this file directly to the command-line arguments.
+  //   // It may be indirectly used within a script file, and the script file itself 
+  //   // is already included as a command-line argument.    
+  // }
 
   void appendFile(const std::string& parameter, const std::string& file)=delete;
-  void appendFile(const std::string& parameter, const std::filesystem::path& file) {
-    m_files.push_back(file);
+  void appendFile(const std::string& parameter, const std::filesystem::path& file, const std::string& role = "") {
+    handleFile(file, role);
     appendArgument(parameter + " "  + file.string());
   }
 
@@ -88,22 +121,22 @@ public:
   }
 
   void prependFile(const std::string& file)=delete;
-  void prependFile(const std::filesystem::path& file) {
-    m_files.push_back(file);
+  void prependFile(const std::filesystem::path& file, const std::string& role = "") {
+    handleFile(file, role);
     prependArgument(file.string());
   }
 
   void prependFile(const std::string& parameter, const std::string& file)=delete;
-  void prependFile(const std::string& parameter, const std::filesystem::path& file) {
-    m_files.push_back(file);
+  void prependFile(const std::string& parameter, const std::filesystem::path& file, const std::string& role) {
+    handleFile(file, role);
     prependArgument(parameter + "_"  + file.string());
   }
 
   bool isValid() const { return !m_string.empty(); }
 
 private:
-  std::vector<std::filesystem::path> m_files;
-  std::vector<std::filesystem::path> m_watchFiles;
+  std::map<std::string, FileIdentity> m_files;
+  //std::set<std::filesystem::path> m_watchFiles;
   std::string m_string;
 
   void appendArgument(const std::string& arg) {
@@ -120,6 +153,42 @@ private:
         m_string = arg + m_string;
     }
   }
+
+  void handleFile(const std::filesystem::path& file, const std::string& role) {
+    std::string key = role.empty()? file.string(): role;
+    m_files[key] = FileIdentity{file, role};
+  }
+
+  bool compareFiles(const std::map<std::string, FileIdentity>& filesMap1, const std::map<std::string, FileIdentity>& filesMap2, std::string& msg) {
+    std::set<std::string> keyDiff = getSymmetricKeyDifference(filesMap1, filesMap2);
+    if (!keyDiff.empty()) {
+      //msg = "changes in following files: " + set2Str(diff);
+      return false;
+    }
+
+    // for ()
+  }
+
+std::set<std::string> getSymmetricKeyDifference(const std::map<std::string, FileIdentity>& a, const std::map<std::string, FileIdentity>& b) 
+{
+  std::set<std::string> diff;
+
+  for (const auto& pair : a) {
+    if (b.find(pair.first) == b.end()) {
+      diff.insert(pair.first);
+    }
+  }
+
+  for (const auto& pair : b) {
+    if (a.find(pair.first) == a.end()) {
+      diff.insert(pair.first);
+    }
+  }
+
+  return diff;
+}
+
 };
+using CommandWrapperPtr = std::shared_ptr<CommandWrapper>;
 
 }  // namespace FOEDAG
