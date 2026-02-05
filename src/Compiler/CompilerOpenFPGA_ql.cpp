3bec414c9ef0d8f8bf3e02a765a4a6367f296f80
Oleksandr <apivovarov@quicklogic.com>
2026-02-05 18:40:01 +0200

    refactor

--- src/Compiler/CompilerOpenFPGA_ql.cpp
+++ src/Compiler/CompilerOpenFPGA_ql.cpp
@@ -5982,16 +5982,10 @@ int status = ExecuteAndMonitorSystemCommand(command);
   ofs << processedScript << std::endl;
   ofs.close();
 
-  Message("bypass LEGACY bitstream command execution " + scriptFilePath.string() + " will be used instead");
-  std::string out;
-
-  assert(TclInterp());
-  out = TclInterp()->evalFile(scriptFilePath.string());
-
-  if (!out.empty()) {
-    Message(out);
-  }
-  int status = false;
+  Message("bypass LEGACY bitstream command execution, use " + scriptFilePath.string() + " instead");
+  int status = Tcl_EvalFile(TclInterp()->getInterp(), scriptFilePath.string().c_str());
+  //std::string out = TclInterp()->evalFile(scriptFilePath.string());
+  //Message(out);
 #endif 
   CleanTempFiles();
   if (status) {
