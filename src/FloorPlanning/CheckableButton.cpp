88d2811b33a652a43c7fbe3bbb76e0367f685b3d
Oleksandr <apivovarov@quicklogic.com>
2026-02-24 12:27:41 +0200

    add UI to collapseAll/expandAll for netlist items

--- src/FloorPlanning/CheckableButton.cpp
+++ src/FloorPlanning/CheckableButton.cpp
@@ -4,33 +4,50 @@
 
 namespace fp {
 
-CheckableButton::CheckableButton(QIcon icon, QWidget* parent)
-: QPushButton(icon, "", parent)
+CheckableButton::CheckableButton(const QIcon& icon, QWidget* parent)
+: QPushButton(icon, "", parent),
+  m_hightLightWhenChecked(true)
 {
   setCheckable(true);
+
+  m_hightLightPen.setColor(m_hightLightColor);
+  m_hightLightPen.setWidthF(m_lineWidth);
+  m_hightLightPen.setJoinStyle(Qt::RoundJoin);
+  m_hightLightPen.setCapStyle(Qt::RoundCap);
+}
+
+CheckableButton::CheckableButton(const QIcon& uncheckedIcon, const QIcon& checkedIcon, QWidget* parent)
+: QPushButton(uncheckedIcon, "", parent),
+  m_uncheckedIcon(uncheckedIcon),
+  m_checkedIcon(checkedIcon),
+  m_hightLightWhenChecked(false)
+{
+  setCheckable(true);
+  connect(this, &QPushButton::toggled, this, [this](bool checked) {
+    if (checked) {
+      setIcon(m_checkedIcon);
+    } else {
+      setIcon(m_uncheckedIcon);
+    }
+  });
 }
 
 void CheckableButton::paintEvent(QPaintEvent* event)
 {
   QPushButton::paintEvent(event);
 
-  if (isChecked()) {
-    QPainter p(this);
-    p.setRenderHint(QPainter::Antialiasing, true);
+  if (m_hightLightWhenChecked) {
+    if (isChecked()) {
+      QPainter p(this);
+      p.setRenderHint(QPainter::Antialiasing, true);
 
-    const qreal line = 3.0;
-    const QColor color(0, 100, 0, 100);
+      p.setBrush(Qt::NoBrush);
+      p.setPen(m_hightLightPen);
 
-    QPen pen(color);
-    pen.setWidthF(line);
-    pen.setJoinStyle(Qt::RoundJoin);
-    pen.setCapStyle(Qt::RoundCap);
-    p.setPen(pen);
-    p.setBrush(Qt::NoBrush);
-
-    QRectF r = QRectF(rect());
-    r.adjust(line * 0.5, line * 0.5, -line * 0.5, -line * 0.5);
-    p.drawRect(r);
+      QRectF r = QRectF(rect());
+      r.adjust(m_lineWidth * 0.5, m_lineWidth * 0.5, -m_lineWidth * 0.5, -m_lineWidth * 0.5);
+      p.drawRect(r);
+    }
   }
 }
 
