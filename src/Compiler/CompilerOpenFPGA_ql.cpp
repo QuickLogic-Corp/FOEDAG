8eed8565d84c8210597aa2d559bab7ace35a8af7
Oleksandr <apivovarov@quicklogic.com>
2026-05-29 20:30:03 +0300

    Delegate file-existence/skip checks to generate_floorplanning.py

--- src/Compiler/CompilerOpenFPGA_ql.cpp
+++ src/Compiler/CompilerOpenFPGA_ql.cpp
@@ -6719,20 +6719,12 @@ bool CompilerOpenFPGA_ql::GenerateIOFloorPlanConstraints(bool forceOverwrite) {
   //m_blifParser.printHierachy(); // debug
   
   auto [pinTableFile, error] = findCurrentDevicePinTableCsv();
-  if (pinTableFile.empty()) 
-      Message(std::string(__func__) + ": pin table csv not found, cannot pass it to the generate_floorplanning.");
 
   std::filesystem::path filepath_fpga_io_map_xml;
   filepath_fpga_io_map_xml = QLDeviceManager::getInstance()->deviceOpenFPGAIOMapFile();
-  if(filepath_fpga_io_map_xml.empty()) {
-    Message(std::string(__func__) + ": fpga io map xml not found, cannot pass it to the generate_floorplanning.");
-  }
 
   std::filesystem::path floor_planning_constraint_filepath = QLSettingsManager::getInstance()->getQDCFilePath();
-  if (!fs::exists(floor_planning_constraint_filepath) && !fs::exists(pinTableFile)){
-    Message("qdc Constraint File and Pin Table File Does Not Exist. Skipping the generate_floorplanning Script.\n");
-    return true;
-  }
+
   std::string region_groups_str = "";
   if (fs::exists(floor_planning_constraint_filepath)) {
     std::unordered_set<std::string> leftSet, rightSet, topSet, bottomSet;
@@ -6901,7 +6893,7 @@ bool CompilerOpenFPGA_ql::GenerateIOFloorPlanConstraints(bool forceOverwrite) {
     return false;
   }
   else if (status == 2){ //Skipped
-    Message("All of the atoms on the QDC have been overwritten by PCF file; Thus, no partition has been created!");
+    Message("IO Floor Plan Generation Skipped!");
     return true;
   }
   else { //Success
